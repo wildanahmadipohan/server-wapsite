d6fa8252ce7d7f74a1314237654b1add44bbe6a9
Wildan Ahmadi Pohan <wildanahmadipohan@gmail.com>
2022-12-20 07:51:25 +0700

    Change Express Session to Cookie Session

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const methodOverride = require('method-override');
-const session = require('express-session');
+// const session = require('express-session');
+const session = require('cookie-session');
 const flash = require('connect-flash');
 const cors = require('cors');
 
